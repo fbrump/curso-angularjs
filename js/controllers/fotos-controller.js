312b6f6a0ec18634e27cb14859f787ba982b3dd8
Felipe Brum <fbrump@gmail.com>
2016-11-30 21:37:14 -0200

    Create the first repeat angular in the index.html

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -1,6 +1,6 @@
 angular.module('alurapic').controller('FotosController', function ($scope) {
 	
-	$scope.foto = [
+	$scope.fotos = [
 		{
 			titulo: 'Leão1',
 			url: 'http://orig13.deviantart.net/06a3/f/2016/032/5/7/lion_by_douglas05-d9q527l.jpg',
