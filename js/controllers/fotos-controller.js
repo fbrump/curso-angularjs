2ce94928c4832070d1bc3e5be4214a003a0d3f4a
Felipe Brum <fbrump@gmail.com>
2016-11-30 21:47:34 -0200

    Add first interation with webservice using $http.get(), then() and catch()

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -2,4 +2,12 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 	
 	$scope.fotos = [];
 
+	var promise = $http.get('v1/fotos');
+
+	promise.then(function (retorno) {
+		$scope.fotos = retorno.data;
+	}).catch(function(error){
+		console.log(error);
+	});
+
 });
