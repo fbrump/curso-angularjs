d25c7370f214b0ced933be2bb2e3faf3f9380724
Felipe Brum <fbrump@gmail.com>
2016-11-30 21:29:47 -0200

    Add new logic for others imagens and panel in the controller

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -4,4 +4,14 @@ angular.module('alurapic').controller('FotosController', function ($scope) {
 		titulo: 'Leão',
 		url: 'http://orig13.deviantart.net/06a3/f/2016/032/5/7/lion_by_douglas05-d9q527l.jpg',
 	};
+
+	$scope.foto2 = {
+		titulo: 'Leão 2',
+		url: 'http://orig13.deviantart.net/06a3/f/2016/032/5/7/lion_by_douglas05-d9q527l.jpg',
+	};
+
+	$scope.foto3 = {
+		titulo: 'Leão 3',
+		url: 'http://orig13.deviantart.net/06a3/f/2016/032/5/7/lion_by_douglas05-d9q527l.jpg',
+	};
 });
