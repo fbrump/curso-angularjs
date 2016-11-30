e7b7105c3fbabed39c96b94463299dba1ffbe4d0
Felipe Brum <fbrump@gmail.com>
2016-11-30 21:53:12 -0200

    Remove the other form for consume $http.get() and implement the .success() and .error() less verbose

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -2,12 +2,12 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 	
 	$scope.fotos = [];
 
-	var promise = $http.get('v1/fotos');
-
-	promise.then(function (retorno) {
-		$scope.fotos = retorno.data;
-	}).catch(function(error){
-		console.log(error);
+	var promise = $http.get('v1/fotos')
+	.success(function (fotos) {
+		$scope.fotos = fotos;
+	})
+	.error(function(erro) {
+		console.error(erro);
 	});
 
 });
