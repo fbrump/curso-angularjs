4aa097147a29ae2db72b865bf9583428cf871ae8
Felipe Brum <fbrump@gmail.com>
2016-11-29 22:55:22 -0200

    add files to start

---
+++ js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function ($scope) {
+	
+	$scope.foto = {
+		titulo: 'Leão',
+		url: 'http://orig13.deviantart.net/06a3/f/2016/032/5/7/lion_by_douglas05-d9q527l.jpg',
+	};
+});
