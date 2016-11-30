090d81983b25c6f22919c70dee31c0bb1d84dc47
Felipe Brum <fbrump@gmail.com>
2016-11-30 21:42:50 -0200

    Remove the informations of the list fotos and instance a empty list

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -1,18 +1,5 @@
 angular.module('alurapic').controller('FotosController', function ($scope, $http) {
 	
-	$scope.fotos = [
-		{
-			titulo: 'Leão1',
-			url: 'http://orig13.deviantart.net/06a3/f/2016/032/5/7/lion_by_douglas05-d9q527l.jpg',
-		},
-		{
-			titulo: 'Leão2',
-			url: 'http://orig13.deviantart.net/06a3/f/2016/032/5/7/lion_by_douglas05-d9q527l.jpg',
-		},
-		{
-			titulo: 'Leão3',
-			url: 'http://orig13.deviantart.net/06a3/f/2016/032/5/7/lion_by_douglas05-d9q527l.jpg',
-		}
-	];
+	$scope.fotos = [];
 
 });
