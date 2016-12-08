6b108bd1775e8ce939b6105300a594b727995edc
Felipe Brum <fbrump@gmail.com>
2016-12-08 21:39:07 -0200

    Add restrict on my directive minhasDiretivas

--- js/directives/minhas-diretivas.js
+++ js/directives/minhas-diretivas.js
@@ -1,1 +1,11 @@
-angular.module('minhasDiretivas', []);
+"use strict"; // reference: http://www.w3schools.com/js/js_strict.asp
+
+angular.module('minhasDiretivas', [])
+.directive('meuPainel', function () {
+	// directive difinition object
+	var ddo = {};
+
+	ddo.restrict = 'AE' // A - attribute; E - Element
+
+	return ddo;
+});
