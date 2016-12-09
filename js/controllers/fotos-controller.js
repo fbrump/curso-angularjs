8e38f7d532bb15bf33b5f5a3ecb89f8de872c85e
Felipe Brum <fbrump@gmail.com>
2016-12-08 22:50:44 -0200

    Add scope.filtro on fotos-controller.js

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -3,6 +3,7 @@
 angular.module('alurapic').controller('FotosController', function ($scope, $http) {
 	
 	$scope.fotos = [];
+	$scope.filtro = '';
 
 	var promise = $http.get('v1/fotos')
 	.success(function (fotos) {
