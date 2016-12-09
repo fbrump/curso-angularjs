dd1c0412db1d814503e4988f226a81023df9f1ed
Felipe Brum <fbrump@gmail.com>
2016-12-08 22:09:17 -0200

    Create external template from file directive

--- js/directives/minhas-diretivas.js
+++ js/directives/minhas-diretivas.js
@@ -1,24 +1,20 @@
 "use strict"; // reference: http://www.w3schools.com/js/js_strict.asp
 
 angular.module('minhasDiretivas', [])
-.directive('meuPainel', function () {
+.directive('meuPainel', [function () {
 	// directive difinition object
 	var ddo = {};
 
-	ddo.restrict = 'AE' // A - attribute; E - Element
+	ddo.restrict = 'AE'; // A - attribute; E - Element
 
 	ddo.scope = {
-		titlo: '@'
+		titulo: '@'
 	};
 
-	ddo.template = 
-			'<div data-ng-repeat="foto in fotos" class="panel panel-default">'
-        +	'    <div class="panel-heading">'
-        +	'        <h3 class="panel-title">{{ titulo }}</h3>'
-        +	'    </div>'
-        +	'    <div class="panel-body">'
-        +	'    </div>'
-        +	'</div>'
+	ddo.transclude = true;
+
+	ddo.templateUrl = 'js/directives/meuPainel.html';
+		
 
 	return ddo;
-});
+}]);
