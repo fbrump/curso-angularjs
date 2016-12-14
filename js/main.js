08aea11d9e9bfa35c0e665ce41f7439688c17234
Felipe Brum <fbrump@gmail.com>
2016-12-14 06:40:50 -0200

    Create second route for view new foto and add foto.html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 "use strict"; // reference: http://www.w3schools.com/js/js_strict.asp
 
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(['$routeProvider', function ($routeProvider) {
+	
+	$routeProvider.when('/fotos', {
+		templateUrl: 'partials/principal.html',
+		controller: 'FotosController'
+	});
+
+	$routeProvider.when('/fotos/new', {
+		templateUrl: 'partials/foto.html',
+		//controller: 'FotosController'
+	});
+
+}]);
