ed4d6ffe8fe7d87b991f743dfca02a3fff664aee
Felipe Brum <fbrump@gmail.com>
2016-12-20 06:38:38 -0200

    Adjusted principal.html, fixed link Nova Foto and build alter scope removed tha item that removed from service

--- js/controllers/fotos-controller.js
+++ js/controllers/fotos-controller.js
@@ -4,6 +4,7 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 	
 	$scope.fotos = [];
 	$scope.filtro = '';
+	$scope.mensagem = '';
 
 	var promise = $http.get('v1/fotos')
 	.success(function (fotos) {
@@ -16,12 +17,14 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
 	$scope.remover = function (foto) {
 		$http.delete('v1/fotos/' + foto._id)
 		.success(function (argument) {
-			console.log('Foto ' + foto.titulo + ' foi removida com sucesso');
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto, 1);
+			$scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso';
 		})
 		.error(function(erro) {
 			/* Act on the event */
 			console.error(erro);
-			console.info('Não foi possível remover a foto ' + foto.titulo);
+			$scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
 		});
 	};
 
