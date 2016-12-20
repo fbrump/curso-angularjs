9ef9af0cf5cfacdf323a9c25b20029c340f61b8f
Felipe Brum <fbrump@gmail.com>
2016-12-20 06:13:00 -0200

    Fineshed controller foto-controller.js with method of insert.

--- js/controllers/foto-controller.js
+++ js/controllers/foto-controller.js
@@ -1,12 +1,26 @@
 // public/js/controllers/foto-controller.js
 "use strict";
 
-angular.module('alurapic').controller('FotoController', ['$scope', function($scope){
+angular.module('alurapic').controller('FotoController', ['$scope', '$http', function($scope, $http){
 	
 	$scope.foto = { };
+	$scope.mensagem = '';
 
 	$scope.submeter = function () {
-		console.log($scope.foto);
+		if ($scope.formulario.$valid){
+			$http.post('v1/fotos', $scope.foto)
+			.success(function () {
+				$scope.foto = {};
+				$scope.mensagem = 'Foto incluida com sucesso';
+			})
+			.error(function(e) {
+				/* Act on the event */
+				$scope.mensagem = 'Não foi possível incluir a foto';
+				console.error(e);
+			});
+		}else{
+			console.info('O formulário não está válido.');
+		}
 	};
 
 }])
